3d647a748f5c617e7ecf24fa38ab512e59610246
Syahmi <163218P@mymail.nyp.edu.sg>
2019-11-16 01:05:51 +0800

    Added listing function to program

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -1,5 +1,24 @@
 pragma solidity ^0.5.0;
 
 contract TodoList {
+    //uint means that it can never be negative
     uint public taskCount = 0; //This is a state variable to write to blockchain
+
+    struct Task {
+        uint id;
+        string content;
+        bool completed;
+    }
+    //Automatically creates a reader func with public
+    mapping(uint => Task) public tasks; //This is a hash table(keyvalue)
+
+    //On Init trigger this
+    constructor() public {
+        createTask("Check out youtube.com");
+    }
+
+    function createTask(string memory _content) public {
+        taskCount ++;
+        tasks[taskCount] = Task(taskCount, _content, false);
+    }
 }
